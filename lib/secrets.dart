8bc6576bca4650aa29f73e1964e10453bfb84604
lkavila <lkavila@uninorte.edu.co>
2021-04-11 11:15:34 -0500

    quitar secrets

--- lib/secrets.dart
+++
@@ -1,5 +0,0 @@
-class Secrets {
-  static final String hereAPIKEY = "YGbDQjCN-FTQsORX4ZAj-W_9Y-3S4J6vyJtCOl6jAt4";
-  static final String googleAPIKEY = "AIzaSyBWiOzMa6XAW9sVCQg7hJPD-YpInHbXLvc";
-  static final String orsAPIKEY = "5b3ce3597851110001cf6248d4141599836d412aa58e4b7569d36ef5";
-}
